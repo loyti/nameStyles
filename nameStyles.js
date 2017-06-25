6c89bd5cffc7a8e0d93be6ff95f43ed98842409f
loyti <brice@loyti.com>
2017-06-25 06:55:11 -0700

    updated html, css & html files

---
+++ nameStyles.js
@@ -0,0 +1,15 @@
+var entry3 = "John";
+var entry2 = "Julie";
+var entry1 = "Nikolai";
+var entry0  = "Nancy";
+var nameStyles = [entry3, entry2, entry1, entry0];
+function styleRun () {
+    document.write("<h2>Playing with styles can be a lot of fun</h2>")
+    for (var i = 0; i < nameStyles.length; i++){
+        document.write("<p class=\"name0\">Check out these names " + nameStyles[i] + " in style number 1.</p>");
+        document.write("<p class=\"name1\">Check out these names " + nameStyles[i] + " in style number 2.</p>");
+        document.write("<p class=\"name2\">Check out these names " + nameStyles[i] + " in style number 3.</p>");
+        document.write("<p class=\"name3\">Check out these names " + nameStyles[i] + " in style number 4.</p>");
+    }    
+}
+styleRun();
