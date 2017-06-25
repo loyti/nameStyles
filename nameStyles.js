eb290b78906ca5f52f380cd2cb17e972b626d85b
loyti <brice@loyti.com>
2017-06-25 06:57:42 -0700

    updated js file

--- nameStyles.js
+++ nameStyles.js
@@ -6,10 +6,10 @@ var nameStyles = [entry3, entry2, entry1, entry0];
 function styleRun () {
     document.write("<h2>Playing with styles can be a lot of fun</h2>")
     for (var i = 0; i < nameStyles.length; i++){
-        document.write("<p class=\"name0\">Check out these names " + nameStyles[i] + " in style number 1.</p>");
-        document.write("<p class=\"name1\">Check out these names " + nameStyles[i] + " in style number 2.</p>");
-        document.write("<p class=\"name2\">Check out these names " + nameStyles[i] + " in style number 3.</p>");
-        document.write("<p class=\"name3\">Check out these names " + nameStyles[i] + " in style number 4.</p>");
+        document.write("<p class=\"name0\">Check out this name " + nameStyles[i] + " in style number 1.</p>");
+        document.write("<p class=\"name1\">Check out this name " + nameStyles[i] + " in style number 2.</p>");
+        document.write("<p class=\"name2\">Check out this name " + nameStyles[i] + " in style number 3.</p>");
+        document.write("<p class=\"name3\">Check out this name " + nameStyles[i] + " in style number 4.</p>");
     }    
 }
 styleRun();
